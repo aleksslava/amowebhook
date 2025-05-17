8ed652b8417bde8334c61dd6e31aecddf53a1feb
Vuacheslav <soloomonslava1991@gmail.com>
2025-05-18 01:39:17 +0400

    .

--- app/main.py
+++ app/main.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI, Body
 import json
 from aiogram import Bot
+from pydantic import BaseModel
+
 from settings.settings import load_config
-from typing import Annotated
+from typing import Annotated, List
 
 config = load_config()
 bot = Bot(token=config.tg_bot.token)
 
 app = FastAPI()
 
+# Определение моделей данных
+class Status(BaseModel):
+    id: int
+    old_pipeline_id: int
+    pipeline_id: int
+    old_status_id: int
+    status_id: int
+
+class Leads(BaseModel):
+    status: List[Status]
+
+class RequestBody(BaseModel):
+    leads: Leads
+
 
 @app.post('/')
-async def get_info(message: Annotated[str, Body()]):
+async def get_info(request_body: RequestBody):
     await bot.send_message(chat_id=config.admin_chat_id,
-                           text=str(message))
+                           text=str(request_body))
