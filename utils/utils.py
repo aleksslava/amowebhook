d3a72e42f07dca7984075f5b9394c0bff5d79f75
Vuacheslav <soloomonslava1991@gmail.com>
2025-05-18 21:15:35 +0400

    .

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 def get_lead_bonus(lst: List):
     if lst is None:
         return 0
-    bonus = [res for res in lst if res['field_id'] == '1105034']
+    bonus = [res for res in lst if res['field_id'] == 1105034]
     logger.info(msg=str(bonus))
     if bonus is None:
         return 0
