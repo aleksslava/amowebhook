f53411647e2d859e456dfc4d228818a88af5aeb4
Vuacheslav <soloomonslava1991@gmail.com>
2025-05-18 22:31:07 +0400

    .

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ def get_lead_bonus(lst: List):
     if lst is None:
         return 0
     bonus = [res for res in lst if res['field_id'] == 1105034]
-    if bonus is None:
+    if not bonus:
         return 0
     bonus = bonus[0].get('values')[0].get('value')
     return bonus
