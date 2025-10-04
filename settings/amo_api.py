b166f5fe0a688396958c0459fafb557826feebd9
Vuacheslav <soloomonslava1991@gmail.com>
2025-10-05 00:18:34 +0300

    .

--- settings/amo_api.py
+++ settings/amo_api.py
@@ -257,18 +257,14 @@ class AmoCRMWrapper:
             logger.error('Нет авторизации в AMO_API')
             return False, 'Произошла ошибка на сервере!'
 
-    def put_full_price_to_customer(self, id_customer, new_price, new_bonus):
+    def put_full_price_to_customer(self, id_customer, new_price):
         url = f'/api/v4/customers/{id_customer}'
         data = {"custom_fields_values": [
             {"field_id": 1105022,
              "values": [
                  {"value": f"{new_price}"},
                  ]
-             },
-            {"field_id": 971580,
-             "values": [
-                 {"value": f"{new_bonus}"},
-             ]}]}
+             }]}
         response = self._base_request(type='patch', endpoint=url, data=data)
 
 
