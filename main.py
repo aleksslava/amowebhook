0ff177f2209bd7608df7875dababa1615159fd3c
Vuacheslav <soloomonslava1991@gmail.com>
2026-04-15 16:04:57 +0300

    .

--- main.py
+++ main.py
@@ -602,7 +602,7 @@ async def get_kp(request: Request, lead_id: int):
     lead_response = await amo_api.get_lead_with_catalog_elements(lead_id=lead_id)
     lead_catalog_elements = get_catalog_elements_from_lead(lead_response)
     project = amo_api._get_custom_field_value(lead_response, project_field_id)
-    responsible_manager_id = lead_response.get("responsible_manager_id")
+    responsible_manager_id = lead_response.get("responsible_user_id")
     responsible_manager = await amo_api.get_responsible_user_by_id(responsible_manager_id)
     responsible_manager_name = responsible_manager.get('name')
 
